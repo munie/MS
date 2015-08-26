e0c53a7d98606d217c88ce92c8608aaf83bfebf5
munie <395412693@qq.com>
2015-08-26 16:49:56 +0800

    Add IDataHandle.cs

--- StationConsole/DataLayer/ClientPointUnit.cs
+++ StationConsole/DataLayer/ClientPointUnit.cs
@@ -6,12 +6,24 @@ using System.Text;
 namespace StationConsole
 {
     [Serializable]
-    public class ClientPointUnit : ClientPoint
+    public class ClientPointUnit : ClientPoint, ICloneable
     {
         public override string RemoteIP { get; set; }
         public override int LocalPort { get; set; }
         public override DateTime ConnectTime { get; set; }
         public override string CCID { get; set; }
         public override string Name { get; set; }
+
+        public object Clone()
+        {
+            ClientPointUnit client = new ClientPointUnit();
+            client.RemoteIP = this.RemoteIP;
+            client.LocalPort = this.LocalPort;
+            client.ConnectTime = this.ConnectTime;
+            client.CCID = this.CCID;
+            client.Name = this.Name;
+
+            return client;
+        }
     }
 }
---
+++ StationConsole/DataLayer/IDataHandle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocketTransfer
+{
+    public interface IDataHandle
+    {
+        int GetDefaultListenPort();
+
+        void InitializeSource();
+
+        void StartListener(System.Net.IPEndPoint ep);
+
+        void StopListener();
+
+        void StartTimerCommand(double interval, string command);
+
+        void StopTimerCommand();
+
+        void SendClientCommand(System.Net.IPEndPoint ep, byte[] data);
+
+        void CloseClient(System.Net.IPEndPoint ep);
+    }
+}
