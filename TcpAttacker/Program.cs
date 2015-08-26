4b6153486dd591c1fa17692b5c285d944eae2051
munie <395412693@qq.com>
2015-08-26 16:27:39 +0800

    Optimize for loading message.xml

--- TcpAttacker/Program.cs
+++ TcpAttacker/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.Xml;
+using System.IO;
 
 namespace TcpAttacker
 {
@@ -19,33 +20,51 @@ namespace TcpAttacker
 
 
             // 读取配置文件
-            XmlDocument xdoc = new XmlDocument();
-            xdoc.Load("config\\message.xml");
+            if (File.Exists(System.AppDomain.CurrentDomain.BaseDirectory + "\\config\\message.xml") == false) {
+                Console.WriteLine("Can't find message.xml at directory config...");
+                Console.WriteLine("Application will be closed in 5 seconds.");
+                System.Threading.Thread.Sleep(5000);
+                return;
+            }
 
-            XmlNode codingNode = xdoc.SelectSingleNode("/configuration/encoding");
-            coding = Encoding.GetEncoding(codingNode.InnerText);
+            try {
+                XmlDocument xdoc = new XmlDocument();
+                xdoc.Load(System.AppDomain.CurrentDomain.BaseDirectory + "\\config\\message.xml");
 
-            XmlNode ipNode = xdoc.SelectSingleNode("/configuration/ipaddress");
-            ipAddress = IPAddress.Parse(ipNode.InnerText);
+                XmlNode codingNode = xdoc.SelectSingleNode("/configuration/encoding");
+                coding = Encoding.GetEncoding(codingNode.InnerText);
 
-            XmlNodeList nodes = xdoc.GetElementsByTagName("station");
-            foreach (XmlNode item in nodes) {
-                int Count = Convert.ToInt32(item.Attributes["count"].InnerText);
+                XmlNode ipNode = xdoc.SelectSingleNode("/configuration/ipaddress");
+                ipAddress = IPAddress.Parse(ipNode.InnerText);
 
-                for (int i = 0; i < Count; i++) {
-                    TcpAttacker attacker = new TcpAttacker();
-                    attacker.Coding = coding;
-                    attacker.Rand = new Random(random.Next(0,10000));
-                    attacker.Name = item.Attributes["name"].Value;
-                    attacker.EP = new IPEndPoint(ipAddress, Convert.ToInt32(item.Attributes["port"].Value));
-                    attacker.Interval = 1000 * random.Next(1, Convert.ToInt32(item.Attributes["max_interval"].Value));
-                    XmlNodeList msg = item.SelectNodes("message");
-                    foreach (XmlNode node in msg) {
-                        attacker.MessageTable.Add(node.Attributes["content"].Value);
+                XmlNodeList nodes = xdoc.GetElementsByTagName("station");
+                foreach (XmlNode item in nodes) {
+                    int Count = Convert.ToInt32(item.Attributes["count"].InnerText);
+
+                    for (int i = 0; i < Count; i++) {
+                        TcpAttacker attacker = new TcpAttacker();
+                        attacker.Coding = coding;
+                        attacker.Rand = new Random(random.Next(0, 10000));
+                        attacker.Name = item.Attributes["name"].Value;
+                        attacker.EP = new IPEndPoint(ipAddress, Convert.ToInt32(item.Attributes["port"].Value));
+                        attacker.Interval = 1000 * random.Next(1, Convert.ToInt32(item.Attributes["max_interval"].Value));
+                        XmlNodeList msg = item.SelectNodes("message");
+                        foreach (XmlNode node in msg) {
+                            attacker.MessageTable.Add(node.Attributes["content"].Value);
+                        }
+                        attackerTable.Add(attacker);
                     }
-                    attackerTable.Add(attacker);
                 }
             }
+            catch (Exception ex) {
+                Console.Write(ex.ToString());
+                Console.WriteLine("");
+                Console.WriteLine("Error: Reading message.xml Fail.");
+                Console.WriteLine("Application will be closed in 5 seconds.");
+                System.Threading.Thread.Sleep(5000);
+                return;
+            }
+
 
             Console.WriteLine("Attacker Command: start | stop | quit");
 
