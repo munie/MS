4cb8fa65d4f52a03eb4464b195e962850b59f5d4
munie <395412693@qq.com>
2015-10-14 22:47:19 +0800

    Add CmdUnit & CmdInputDialog to SockMgr

--- SockMgr/SockMgrWindow.xaml.cs
+++ SockMgr/SockMgrWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace SockMgr
         public static readonly string conf_name = "sockmgr.xml";
         private SockSessManager sessmgr;
         public ObservableCollection<SockUnit> SockTable { get; set; }
+        public ObservableCollection<CmdUnit> CmdTable { get; set; }
 
         private void init()
         {
@@ -50,7 +51,8 @@ namespace SockMgr
 
             // init SockTable
             SockTable = new ObservableCollection<SockUnit>();
-            DataContext = new { SockTable = SockTable };
+            CmdTable = new ObservableCollection<CmdUnit>();
+            DataContext = new { SockTable = SockTable, CmdTable = CmdTable };
         }
 
         private void config()
@@ -80,6 +82,16 @@ namespace SockMgr
                         sock.Comment = sock.EP.ToString() + " C " + SockUnit.StateClosed;
                     SockTable.Add(sock);
                 }
+
+                // command
+                foreach (XmlNode item in doc.SelectNodes("/configuration/command/cmditem")) {
+                    CmdUnit cmd = new CmdUnit();
+                    cmd.ID = item.Attributes["id"].Value;
+                    cmd.Name = item.Attributes["name"].Value;
+                    cmd.Cmd = item.Attributes["content"].Value;
+                    cmd.Comment = item.Attributes["comment"].Value;
+                    CmdTable.Add(cmd);
+                }
             }
             catch (Exception ex) {
                 Mnn.MnnUtil.Logger.WriteException(ex);
@@ -90,6 +102,28 @@ namespace SockMgr
 
         private void sessmgr_sess_parse(object sender, SockSess sess)
         {
+            Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                if (txtMsg.Text.Length >= 20 * 1024) {
+                    txtMsg.Clear();
+                }
+
+                string hexstr = "";
+                foreach (var item in sess.rdata.Take(sess.rdata_size).ToArray()) {
+                    string s = Convert.ToString(item, 16);
+                    if (s.Length == 1)
+                        s = "0" + s;
+                    if (hexstr.Length != 0)
+                        hexstr += " " + s;
+                    else
+                        hexstr += s;
+                }
+
+                txtMsg.AppendText(hexstr + "\n");
+                txtMsg.ScrollToEnd();
+
+                sess.rdata_size = 0;
+            }));
         }
 
         private void sessmgr_sess_create(object sender, SockSess sess)
@@ -137,7 +171,7 @@ namespace SockMgr
 
         private void MenuItem_Listen_Click(object sender, RoutedEventArgs e)
         {
-            SockUnit sock = TreeSock.SelectedItem as SockUnit;
+            SockUnit sock = treeSock.SelectedItem as SockUnit;
 
             if (sock != null && sock.Type == SockUnit.TypeListen &&
                 sock.Comment.Contains(SockUnit.StateClosed) && sessmgr.AddListenSession(sock.EP))
@@ -146,7 +180,7 @@ namespace SockMgr
 
         private void MenuItem_Connect_Click(object sender, RoutedEventArgs e)
         {
-            SockUnit sock = TreeSock.SelectedItem as SockUnit;
+            SockUnit sock = treeSock.SelectedItem as SockUnit;
 
             if (sock != null && sock.Type == SockUnit.TypeConnect &&
                 sock.Comment.Contains(SockUnit.StateClosed) && sessmgr.AddConnectSession(sock.EP))
@@ -155,7 +189,7 @@ namespace SockMgr
 
         private void MenuItem_Close_Click(object sender, RoutedEventArgs e)
         {
-            SockUnit sock = TreeSock.SelectedItem as SockUnit;
+            SockUnit sock = treeSock.SelectedItem as SockUnit;
 
             if (sock != null)
                 sessmgr.RemoveSession(sock.EP);
@@ -165,8 +199,6 @@ namespace SockMgr
             else if (sock.Type == SockUnit.TypeConnect)
                 sock.Comment = sock.Comment.Replace(SockUnit.StateConnected, SockUnit.StateClosed);
         }
-
-        // other methods ============================================================================
 
         private void TreeView_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -183,6 +215,114 @@ namespace SockMgr
                 source = VisualTreeHelper.GetParent(source);
 
             return source;
+        }
+
+        // Cmd methods ======================================================================
+
+        private void MenuItem_SendCmd_Click(object sender, RoutedEventArgs e)
+        {
+            if (treeSock.SelectedItem == null)
+                return;
+
+            SockUnit sock = treeSock.SelectedItem as SockUnit;
+
+            foreach (CmdUnit item in lstViewCmd.SelectedItems) {
+                foreach (var i in sessmgr.sess_table) {
+                    if (i.ep.Equals(sock.EP)) {
+                        if (i.type != SessType.listen)
+                            i.sock.Send(Mnn.MnnUtil.ConvertUtil.CmdstrToBytes(item.Cmd));
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void MenuItem_EditCmd_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstViewCmd.SelectedItems.Count == 0)
+                return;
+
+            using (CmdInputDialog input = new CmdInputDialog()) {
+                input.Owner = this;
+                input.Title = "编辑命令";
+                input.textBoxID.Text = (lstViewCmd.SelectedItems[0] as CmdUnit).ID;
+                input.textBoxName.Text = (lstViewCmd.SelectedItems[0] as CmdUnit).Name;
+                input.textBoxCmd.Text = (lstViewCmd.SelectedItems[0] as CmdUnit).Cmd;
+                input.textBoxComment.Text = (lstViewCmd.SelectedItems[0] as CmdUnit).Comment;
+                input.textBoxCmd.Focus();
+                input.textBoxCmd.SelectionStart = input.textBoxCmd.Text.Length;
+
+                if (input.ShowDialog() == false)
+                    return;
+
+                foreach (CmdUnit item in lstViewCmd.SelectedItems) {
+                    item.ID = input.textBoxID.Text;
+                    item.Name = input.textBoxName.Text;
+                    item.Cmd = input.textBoxCmd.Text;
+                    item.Comment = input.textBoxComment.Text;
+                    break;
+                }
+            }
+        }
+
+        private void MenuItem_NewCmd_Click(object sender, RoutedEventArgs e)
+        {
+            using (CmdInputDialog input = new CmdInputDialog()) {
+                input.Owner = this;
+                input.Title = "新增命令";
+                input.textBoxID.Focus();
+
+                if (input.ShowDialog() == false)
+                    return;
+
+                CmdUnit cmd = new CmdUnit();
+                cmd.ID = input.textBoxID.Text;
+                cmd.Name = input.textBoxName.Text;
+                cmd.Cmd = input.textBoxCmd.Text;
+                cmd.Comment = input.textBoxComment.Text;
+                CmdTable.Add(cmd);
+            }
+        }
+
+        private void MenuItem_DelCmd_Click(object sender, RoutedEventArgs e)
+        {
+            List<CmdUnit> tmp = new List<CmdUnit>();
+
+            foreach (CmdUnit item in lstViewCmd.SelectedItems)
+                tmp.Add(item);
+
+            foreach (var item in tmp)
+                CmdTable.Remove(item);
+        }
+
+        private void MenuItem_SaveCmd_Click(object sender, RoutedEventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode config;
+
+            if (File.Exists(base_dir + conf_name)) {
+                doc.Load(base_dir + conf_name);
+                config = doc.SelectSingleNode("/configuration/command");
+            }
+            else {
+                doc.AppendChild(doc.CreateXmlDeclaration("1.0", "utf-8", ""));
+                XmlElement root = doc.CreateElement("configuration"); // 创建根节点album
+                doc.AppendChild(root);
+                config = doc.CreateElement("command"); // 创建根节点album
+                root.AppendChild(config);
+            }
+
+            config.RemoveAll();
+            foreach (var item in CmdTable) {
+                XmlElement cmd = doc.CreateElement("cmditem");
+                cmd.SetAttribute("id", item.ID);
+                cmd.SetAttribute("name", item.Name);
+                cmd.SetAttribute("content", item.Cmd);
+                cmd.SetAttribute("comment", item.Comment);
+                config.AppendChild(cmd);
+            }
+
+            doc.Save(base_dir + conf_name);
         }
     }
 }
