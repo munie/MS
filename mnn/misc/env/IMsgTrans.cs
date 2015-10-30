24ff2361f72003f91be1812f6e3cf68f4ad1716d
munie <395412693@qq.com>
2015-10-30 15:38:51 +0800

    Add IMsgProc

---
+++ mnn/misc/env/IMsgTrans.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mnn.misc.env {
+    public class SMsgTrans {
+        public static readonly string FULL_NAME = "mnn.misc.env.IMsgTrans";
+        public static readonly string TRANSLATE = "Translate";
+    }
+
+    public interface IMsgTrans {
+        string Translate(string msg);
+    }
+}
