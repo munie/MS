cf95d29670ee6eac92e393ef15af714a27c315c8
munie <395412693@qq.com>
2015-12-16 17:26:06 +0800

    Add ModuleFlow ...

--- mnn/misc/env/EnvConst.cs
+++ mnn/misc/env/EnvConst.cs
@@ -5,14 +5,43 @@ using System.Text;
 
 namespace mnn.misc.env {
     public class EnvConst {
+        /// <summary>
+        /// base directory
+        /// </summary>
         public static readonly string BASE_DIR = System.AppDomain.CurrentDomain.BaseDirectory;
 
+        /// <summary>
+        /// module path
+        /// </summary>
         public static readonly string Module_PATH = BASE_DIR + "Modules";
 
+        /// <summary>
+        /// configuration file path
+        /// </summary>
         public static readonly string CONF_NAME = "EnvConsole.xml";
         public static readonly string CONF_PATH = BASE_DIR + CONF_NAME;
         public static readonly string CONF_ENCODING = "/configuration/encoding";
         public static readonly string CONF_SERVER = "/configuration/servers/server";
         public static readonly string CONF_TIMER = "/configuration/timers/timer";
+
+        /// <summary>
+        /// common msg keys
+        /// </summary>
+        public static readonly string CCID = "ccid";
+        public static readonly string CSQ = "csq";
+        public static readonly string VOLTAGE = "voltage";
+        public static readonly string GPSE = "gpse";
+        public static readonly string GPSN = "gpsn";
+        public static readonly string TIME = "time";
+        public static readonly string ALARM = "alarm";
+        public static readonly string MLSTR = "mlstr";
+        public static readonly string REPLY = "reply";
+
+        /// <summary>
+        /// flow msg keys
+        /// </summary>
+        public static readonly string FLOW_TOTAL = "flow_total";
+
+        public static readonly string OK = "*OK#";
     }
 }
