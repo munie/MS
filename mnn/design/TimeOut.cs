02ae60729a6c6a8306d4fcb61865ad25a1c5a0e1
munie <395412693@qq.com>
2016-01-11 09:12:35 +0800

    Rewrite TimeOut.cs

--- mnn/design/TimeOut.cs
+++ mnn/design/TimeOut.cs
@@ -5,30 +5,45 @@ using System.Text;
 using System.Threading;
 
 namespace mnn.design {
-    /*public*/ class TimeOut {
-        public DateTime TheTimeOut { get; /*private*/ set; }
+    public class TimeOut {
+        public DateTime TheTimeOut { get; protected set; }
         public Delegate Func { get; private set; }
         public object[] Args { get; private set; }
-        public bool Repeat { get; private set; }
-        public int Interval { get; private set; }
 
-        public TimeOut(DateTime timeout, Delegate func, object[] args, bool repeat = false, int interval = 1)
+        public TimeOut(DateTime timeout, Delegate func, object[] args)
         {
             TheTimeOut = timeout;
             Func = func;
             Args = args;
-            Repeat = repeat;
+        }
+    }
+
+    public class Timer : TimeOut {
+        public int Interval { get; private set; }
+
+        public Timer(DateTime timeout, Delegate func, object[] args, int interval)
+            : base(timeout, func, args)
+        {
             Interval = interval >= 1 ? interval : 1;
+        }
+
+        public void UpdateTimeOut()
+        {
+            do {
+                TheTimeOut += new TimeSpan(Interval);
+            } while (TheTimeOut > DateTime.Now);
         }
     }
 
     public class TimeOutCtl {
-        private List<TimeOut> cmd_table;
+        private List<TimeOut> timeout_table;
+        private List<Timer> timer_table;
         private Thread thread;
 
         public TimeOutCtl()
         {
-            cmd_table = new List<TimeOut>();
+            timeout_table = new List<TimeOut>();
+            timer_table = new List<Timer>();
         }
 
         // Methods =============================================================================
@@ -37,17 +52,22 @@ namespace mnn.design {
         {
             ThreadCheck(true);
 
-            lock (cmd_table) {
-                foreach (var item in cmd_table.ToArray()) {
+            // timeout
+            lock (timeout_table) {
+                foreach (var item in timeout_table.ToArray()) {
                     if (DateTime.Now >= item.TheTimeOut) {
-                        item.Func.Method.Invoke(item.Func.Target, null);
-                        if (!item.Repeat)
-                            cmd_table.Remove(item);
-                        else {
-                            do {
-                                item.TheTimeOut += new TimeSpan(item.Interval);
-                            } while (item.TheTimeOut > DateTime.Now);
-                        }
+                        item.Func.Method.Invoke(item.Func.Target, item.Args);
+                        timeout_table.Remove(item);
+                    }
+                }
+            }
+
+            // timer
+            lock (timer_table) {
+                foreach (var item in timer_table) {
+                    if (DateTime.Now >= item.TheTimeOut) {
+                        item.Func.Method.Invoke(item.Func.Target, item.Args);
+                        item.UpdateTimeOut();
                     }
                 }
             }
@@ -58,18 +78,27 @@ namespace mnn.design {
             span = span >= 1 ? span : 1;
             if (func == null) return;
 
-            lock (cmd_table) {
-                cmd_table.Add(new TimeOut(DateTime.Now + new TimeSpan(span), func, args));
+            lock (timeout_table) {
+                timeout_table.Add(new TimeOut(DateTime.Now + new TimeSpan(span), func, args));
             }
         }
 
-        public void AddTimer(int interval, Delegate func, params object[] args)
+        public Timer AddTimer(int interval, Delegate func, params object[] args)
         {
             interval = interval >= 1 ? interval : 1;
-            if (func == null) return;
+            if (func == null) return null;
 
-            lock (cmd_table) {
-                cmd_table.Add(new TimeOut(DateTime.Now + new TimeSpan(interval), func, args, true, interval));
+            lock (timer_table) {
+                Timer timer = new Timer(DateTime.Now + new TimeSpan(interval), func, args, interval);
+                timer_table.Add(timer);
+                return timer;
+            }
+        }
+
+        public void DelTimer(Timer timer)
+        {
+            lock (timer_table) {
+                timer_table.Remove(timer);
             }
         }
 
